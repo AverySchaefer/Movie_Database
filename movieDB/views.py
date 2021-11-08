8ff1142be9e62aebb9b2ecf3c010126f6aac8289
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2021-11-07 23:16:06 -0500

    creating html and view for index page

--- movieDB/views.py
+++ movieDB/views.py
@@ -8,5 +8,14 @@ def home_view(request):
     name = "Avery"
     P_STRING = f"""<p>Hello {name}</p>"""
     HTML_STRING = H1_STRING + P_STRING
-    
-    return HttpResponse(HTML_STRING)
+
+    #Generate the number of movies in the database
+    num_movies = Movie.objects.all().count()
+    num_unseen_movies = Movie.objects.filter(seen__exact=False).count()
+
+    context = {
+        'num_movies': num_movies,
+        'num_unseen_movies': num_unseen_movies,
+    }
+
+    return HttpResponse(request, 'index.html', content=context)
